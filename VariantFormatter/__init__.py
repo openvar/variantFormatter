a59a3e30cfd6ea0773569b894f956fee1d2a3638
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2025-07-22 19:15:00 +0100

    remove pkg_resources

--- VariantFormatter/__init__.py
+++ VariantFormatter/__init__.py
@@ -1,14 +1,14 @@
-import pkg_resources
+import importlib.metadata
 import re
 import warnings
 
 # Pull in use_scm_version=True enabled version number
 _is_released_version = False
 try:
-    __version__ = pkg_resources.get_distribution("VariantFormatter").version
+    __version__ = importlib.metadata.version("VariantFormatter")
     if re.match(r"^\d+\.\d+\.\d+$", __version__) is not None:
         _is_released_version = True
-except pkg_resources.DistributionNotFound as e:
+except importlib.metadata.PackageNotFoundError:
     warnings.warn("can't get __version__ because %s package isn't installed" % __package__, Warning)
     __version__ = None
 
